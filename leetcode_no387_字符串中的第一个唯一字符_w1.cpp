6435909da5505051e88098d468f6216bc5098b82
BestBlade <68151785+BestBlade@users.noreply.github.com>
2020-12-24 11:11:07 +0800

    Add files via upload

---
+++ leetcode_no387_字符串中的第一个唯一字符_w1.cpp
@@ -0,0 +1,64 @@
+﻿/* ------------------------------------------------------------------|
+给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。
+
+ 
+
+示例：
+
+s = "leetcode"
+返回 0
+
+s = "loveleetcode"
+返回 2
+ 
+
+提示：你可以假定该字符串只包含小写字母。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/first-unique-character-in-a-string
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*	双指针法
+*
+*	执行用时：24 ms, 在所有 C++ 提交中击败了95.09%的用户
+*	内存消耗：10.7 MB, 在所有 C++ 提交中击败了63.04%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+int firstUniqChar(string s) {
+    int count[26] = { 0 };
+    for (char c : s) {
+        count[c - 'a']++;
+    }
+    for (int i = 0; i < s.size(); i++) {
+        if (count[s[i] - 'a'] == 1) {
+            return i;
+        }
+    }
+    return -1;
+}
